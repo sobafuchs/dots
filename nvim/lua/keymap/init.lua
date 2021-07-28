d27a98a291eab96c4e02eaca3598416ce1e37c59
Varun Jayaraman <varun.jayaraman@gmail.com>
2021-07-28 12:41:48 +0200

    Vim/lsp enhancements (#49)
    * Group lsp functionality
    
    * Add additional lsp utilities
    
    * Remove comment

--- nvim/lua/keymap/init.lua
+++ nvim/lua/keymap/init.lua
@@ -1,7 +1,4 @@
-local wk = require("which-key")
 local vim = vim
-
-register = wk.register
 
 -- write to buffer
 vim.cmd [[nno ;; :w<CR>]]
---
+++ nvim/lua/plugins/lsp/keymap.lua
@@ -0,0 +1,41 @@
+local vim = vim
+local r = require("which-key").register
+
+local function define_mappings(bufnr)
+  vim.api.nvim_command('nnoremap <silent>K <cmd>:Lspsaga hover_doc<CR>')
+  vim.api.nvim_command("nnoremap <silent> <C-f> <cmd>lua require('lspsaga.action').smart_scroll_with_saga(1)<cr>")
+  vim.api.nvim_command("nnoremap <silent> <C-b> <cmd>lua require('lspsaga.action').smart_scroll_with_saga(-1)<cr>")
+
+  r({
+    ["<Leader>q"] = {'<cmd>lua vim.lsp.diagnostic.set_loclist()<CR>', "diagnostics list"}
+  }, { buffer = bufnr})
+
+  r({
+    l = {
+      name = "lsp",
+      g = {
+        name = "go to",
+        d = {"<Cmd>lua vim.lsp.buf.definition()<CR>", "go to definition"},
+        r = {"<Cmd>lua vim.lsp.buf.references()<CR>", "references"},
+        i = {"<Cmd>lua vim.lsp.buf.implementation()<CR>", "implementation"},
+      },
+      p = {
+        name = "peek",
+        d = {"<Cmd>:Lspsaga preview_definition<CR>", "preview definition"},
+      },
+      r = {
+        name = "refactor",
+        a = {"<Cmd>:Lspsaga code_action<CR>", "code action"},
+        r = {"<Cmd>:Lspsaga rename<CR>", "rename"}
+      }
+    },
+  }, { buffer = bufnr, prefix = "" })
+
+  r({
+    ["<C-p>"] = {"<Cmd>:Lspsaga diagnostic_jump_prev<CR>", "Jump to Previous Error"},
+    ["<C-n>"] = {"<Cmd>:Lspsaga diagnostic_jump_next<CR>", "Jump to Next Error"},
+  }, { buffer = bufnr, prefix = ""})
+
+end
+
+return define_mappings
