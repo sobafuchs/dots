5eb8a329595cf2f3977e4a10f0079c8deef0c4cc
Varun Jayaraman <varun.jayaraman@gmail.com>
2021-12-17 19:05:27 +0100

    Fix annoying nvim tree error

--- nvim/lua/plugins/nvim-tree.lua
+++ nvim/lua/plugins/nvim-tree.lua
@@ -4,7 +4,7 @@ local r = require("which-key").register
 
 require("nvim-tree.events").on_nvim_tree_ready(function() vim.cmd("NvimTreeRefresh") end)
 
-g.nvim_tree_follow = 1
+-- g.nvim_tree_follow = 1
 g.nvim_tree_indent_markers = 1
 g.nvim_tree_icons = {
   default =  '',
