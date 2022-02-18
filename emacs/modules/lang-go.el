527b2c7d2b32a1a770c44cdde82951da75e3142f
Varun Jayaraman <varun.jayaraman@gmail.com>
2022-02-18 20:31:19 +0100

    Add some extra go utilities
    Packages added include:
    
    - go-dlv: for debugging and dap mode in the future
    - go-playground: for playing with quick playground scratch files

--- emacs/modules/lang-go.el
+++ emacs/modules/lang-go.el
@@ -14,6 +14,16 @@
 (use-package go-gen-test)
 (use-package flycheck-golangci-lint)
 (use-package gorepl-mode)
+(use-package gotest
+  :bind (:map go-mode-map
+              ("C-c t f" . go-test-current-file)
+              ("C-c t t" . go-test-current-test)))
+
+(use-package go-playground
+  :diminish
+  :commands (go-playground-mode))
+
+(use-package go-dlv)
 
 (provide 'lang-go)
 ;;; lang-go.el ends here
