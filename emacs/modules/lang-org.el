e7276c3d18b1384ab4159454150f108f4ea67d17
Varun Jayaraman <varun.jayaraman@gmail.com>
2022-02-24 17:56:16 +0100

    Clean up org mode hook
    I am not a huge fan of separating out the setup function and just naming
    it "setup-hook". I don't think it adds much value and lambda () is clear
    enough to me.

--- emacs/modules/lang-org.el
+++ emacs/modules/lang-org.el
@@ -4,11 +4,6 @@
 
 ;;; Code:
 
-(defun sm--org-mode-setup ()
-  (org-indent-mode)
-  (variable-pitch-mode 1)
-  (visual-line-mode 1))
-
 (use-package org
   :config
   (setq org-agenda-start-with-log-mode t) ;; presents log in daily planner view
@@ -17,7 +12,9 @@
   (org-log-into-drawer t)
   (org-directory "~/storage/org")
   (org-agenda-files '("~/storage/org/agenda.org"))
-  :hook (org-mode . sm--org-mode-setup))
+  :hook (org-mode . (lambda ()
+                      (org-indent-mode)
+                      (visual-line-mode 1))))
 
 (use-package org-bullets
   :hook (org-mode . org-bullets-mode)
