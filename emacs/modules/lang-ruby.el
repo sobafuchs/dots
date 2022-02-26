830e6af2630e9dc590f4363c4f9b714de78eeb37
Varun Jayaraman <varun.jayaraman@gmail.com>
2022-02-26 09:24:26 +0100

    Configure inf-ruby

--- emacs/modules/lang-ruby.el
+++ emacs/modules/lang-ruby.el
@@ -7,13 +7,15 @@
 ;; TODO: Move yas snippets to its own module
 
 ;; Ruby REPL
-(use-package inf-ruby)
+(use-package inf-ruby
+  :hook ((ruby-mode . inf-ruby-minor-mode)))
 
 ;; Ruby mode
 (use-package ruby-mode
   :hook ((ruby-mode . lsp-deferred)
          (ruby-mode . smartparens-mode)))
 
+(use-package rbs-mode)
 (provide 'lang-ruby)
 
 ;;; lang-ruby.el ends here
